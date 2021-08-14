3c932c72f95a96e7e4d7790f5a48c4bb63348291
Usman Ul Muazzam <usmaan.rn@gmail.com>
2021-08-15 02:40:47 +0530

    updated Elliott function

--- src/RNSpectra.jl
+++ src/RNSpectra.jl
@@ -1,5 +1,32 @@
 module RNSpectra
+using LsqFit, QuadGK, Plots
 
-# Write your package code here.
+
+
+function ElliottToyozawa(α,En,p0=[1,0.08,5.1,0.2,0.2])
+
+    αf(E,p) = ( p[1].*sqrt( abs(p[2]) )./(E.*(2π) )).*(sum(m-> (2*p[2]/(m^3))*(1/p[4])*exp.(-((E .- (p[3] - p[2]/(m^2))).^2)/(2*(p[4]^2))),1:1000  ) .+ (1/p[5]).*quadgk(x-> exp.(-(x .- E).^2/(2*p[5].^2))./(1 .- exp.(-2π*sqrt( abs(p[2])./(abs(x .- p[3]) ))) ),p[3],Inf,rtol=1e-6)[1]  )
+    αₙₒᵣₘ = α/maximum(α)
+    p0=[1,0.08,5.1,0.2,0.2] # [c0 R Eg Γm Γc]
+    fit=curve_fit(αf,vec(En),αₙₒᵣₘ,p0)
+    cf=coef(fit)
+    J=fit.jacobian
+    R=fit.resid
+
+    return vec(cf), J, R, vec(αf(En,cf))
+end
+
+function ElliottPlot(En,α,kn)
+        αₙₒᵣₘ = α/maximum(α)
+        plotly()
+        plot()
+        p1=plot(vec(En),vec(αₙₒᵣₘ)*maximum(α))
+        p2=scatter!(vec(En),vec(kn*maximum(α)),dpi=1200,legend=:false)
+        return p2
+        
+end
+    
+
+export ElliottToyozawa, ElliottPlot
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using RNSpectra
 using Test
+using DataFrames,DelimitedFiles,Glob
 
 @testset "RNSpectra.jl" begin
-    # Write your tests here.
+    f=Glob.glob("*.txt")
+    dx=readdlm(f[1],',',skipstart=2)
+    df=DataFrame(dx,:auto)
+    Abn=df[:,2]
+    λ = df[:,1]
+    En = 1240*λ.^-1
+    t=798e-7 # nm
+    α = log(10)*Abn/t
+    ElliottToyozawa(α,En)
+
+    cf=ElliottToyozawa(α,En)[4]
+
+    
+    
+
+
+    
 end
